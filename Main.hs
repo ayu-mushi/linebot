d75085f8b72eda19d87a89f6504ab25a2ef8f79c
ayu-mushi <ayu.mushi2015@gmail.com>
2017-12-15 06:56:24 +0900

    modify main to sample

--- Main.hs
+++ Main.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Web.Scotty
+import System.Environment
 
 main :: IO ()
-main = scotty 3000 $ do
-  get "/:word" $ do
-    beam <- param "word"
-    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+main = do
+  env <- getEnvironment
+  let port = maybe 8080 read $ lookup "PORT" env
+  scotty port $ do
+    get "/" $ html $ "Hello, Heroku!"
