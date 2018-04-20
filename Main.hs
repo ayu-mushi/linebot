c613a4ef1c386d11a813291e13de137376c80efd
ayu-mushi <ayu.mushi2015@gmail.com>
2018-04-20 10:33:33 +0900

    JSON

--- Main.hs
+++ Main.hs
@@ -5,14 +5,19 @@ import System.Environment
 import Data.Monoid((<>))
 import Network.HTTP.Types.Status()
 import System.Random
-import qualified Data.Text.Lazy as Text(pack, unpack)
+import qualified Data.Text.Lazy as Text(pack, unpack, Text)
 import Data.Text.IO as Text(writeFile)
 import qualified Data.ByteString.Lazy as BS (pack, unpack, writeFile, readFile)
 import qualified Data.Text.Encoding  as Text(decodeUtf8)
 import Control.Monad.Trans(liftIO)
 import Control.Exception (try, IOException)
+import Control.Monad.Catch as MonadCatch (catch, try, MonadCatch(..), Exception, MonadThrow, throwM)
 import System.Directory (removeFile)
 import qualified Data.ByteString.Lazy.UTF8 as BsUtf8 (foldl, toString)
+import Data.Aeson hiding (json)
+import Control.Monad (mplus, mzero)
+import Web.Scotty.Trans(ScottyError)
+import Web.Scotty.Internal.Types(ActionT(runAM, ActionT), ActionError)
 
 
 {-import Control.Lens ((^?))
@@ -49,13 +54,53 @@ main = do
     get "/json" $ do
       json [(0::Int)..10]
     get "/line" $ do
-      lr <- liftIO $ try $ readFile "/tmp/linerequest.json"
+      lr <- liftIO $ MonadCatch.try $ readFile "/tmp/linerequest.json"
       case lr of
         Right lr' -> text $ Text.pack lr'
         Left (e::IOException) -> text "File not found."
     post "/callback" $ do
+      (linereq::Either Text.Text LINEReq) <- MonadCatch.try jsonData
+      liftIO $ Prelude.writeFile "/tmp/linerequest.json" $ show linereq
       b <- body
       liftIO $ Prelude.writeFile "/tmp/linerequest.json" $ BsUtf8.toString b
+
+data LINEReq = Events { fromLINEReq :: [LINEEvent] } deriving Show
+
+data LINEEvent = LINEEvent {
+  evType :: String
+  , evReplyToken :: String
+  , evTimeStamp:: Int
+  , evMessage :: Message
+  } deriving Show
+
+data Message = Message {
+  msType :: String
+  , msId :: Int
+  , msText :: String
+  } deriving Show
+
+instance FromJSON LINEReq where
+  parseJSON (Object v) = do
+    (arr::[LINEEvent]) <- v .: "events"
+    return $ Events arr
+
+instance FromJSON Message where
+  parseJSON (Object v) = do
+    t <- v .: "type"
+    i <- v .: "id"
+    text <- v .: "text"
+    return $ Message t i text
+  parseJSON _ = mzero
+
+instance FromJSON LINEEvent where
+  parseJSON (Object v) = do
+    t <- v .: "type"
+    rt <- v .: "replyToken"
+    ts <- v .: "timestamp"
+    ms <- v .: "message"
+    return $ LINEEvent t rt ts ms
+
+  parseJSON _ = mzero
 
 {-
 {
@@ -85,3 +130,11 @@ main = do
             }
             ]
             }-}
+
+instance (MonadThrow m, ScottyError e) => MonadThrow (ActionT e m) where
+    throwM = ActionT . throwM
+
+instance (MonadCatch m, ScottyError e) => MonadCatch (ActionT e m) where
+  catch (ActionT m) f = ActionT $ m `MonadCatch.catch` (runAM . f)
+
+instance Exception Text.Text
